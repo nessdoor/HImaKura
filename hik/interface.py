ffdb6bec5a1a281913d941c0d160f8e9521ea91d
Tomas Antonio Lopez <tomasantonio97@gmail.com>
2020-03-25 00:24:49 +0100

    Made the hide_on_delete more general

--- hik/interface.py
+++ hik/interface.py
@@ -15,7 +15,7 @@ dir_selector = builder.get_object("DirectoryOpener")
 
 signal_mapping = {'show_dir_selector': lambda *args: dir_selector.show_all(),
                   'hide_dir_selector': lambda *args: dir_selector.hide(),
-                  'hod_dir_selector': lambda *args: dir_selector.hide_on_delete(),
+                  'hide_on_delete': lambda *args: args[0].hide_on_delete(),
                   'quit_program': lambda *args: Gtk.main_quit()}
 
 builder.connect_signals(signal_mapping)
