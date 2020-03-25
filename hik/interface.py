67543f14889ce07ed569ae57958f0dad5a67e90a
Tomas Antonio Lopez <tomasantonio97@gmail.com>
2020-03-25 01:08:16 +0100

    Finished implementing the dir selector frontend

--- hik/interface.py
+++ hik/interface.py
@@ -3,18 +3,25 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
-
-def quit_program(*args):
-    Gtk.main_quit()
-
-
 builder = Gtk.Builder()
 builder.add_from_file("HImaKura.glade")
 main_window = builder.get_object("MainWindow")
 dir_selector = builder.get_object("DirectoryOpener")
 
+# select_dir = None
+select_dir = lambda *args: None
+
+
+def change_selected_dir(button):
+    # TODO complete this implementation after implementing a proper view
+    # selected_dir = lambda *args: dir_selector.get_filename()
+    button.set_sensitive(True)
+
+
 signal_mapping = {'show_dir_selector': lambda *args: dir_selector.show_all(),
                   'hide_dir_selector': lambda *args: dir_selector.hide(),
+                  'change_dir': change_selected_dir,
+                  'set_as_dir': select_dir,
                   'hide_on_delete': lambda *args: args[0].hide_on_delete(),
                   'quit_program': lambda *args: Gtk.main_quit()}
 
